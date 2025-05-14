30c98e52555e3f1593539f36dcf38995bb8148ea
Suryansh10057 <139328478+Suryansh10057@users.noreply.github.com>
2025-05-14 05:15:01 +0000

    function

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,31 @@
+// var c = 300
+let a = 300
+const b=20
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    var c = 300 //can be accessed from out of this if
+    
+}
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+one()
