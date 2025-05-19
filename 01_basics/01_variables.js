6059678d415990bcc6a609303c7934cd30391f60
Suryansh10057 <139328478+Suryansh10057@users.noreply.github.com>
2025-05-19 13:48:19 +0000

    01

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,11 +3,9 @@ let accountemail="surya@gmsil.com"
 var accountpassword="12345"
 accountcity="Jaipur"
 
-<<<<<<< HEAD
-accountID=1234 //not allowed as const cannot change
 
 console.log(accountID)
-=======
+
 // accountID=1234 //not allowed as const cannot change
 
 console.log(accountID)
@@ -25,4 +23,3 @@ console.table([accountID,accountemail,accountpassword,accountcity,accountstate])
 Prefer not to use var
 because of issue in block scope and functional scope
 */
->>>>>>> 4910cda4c665fd3a2d6421bcd623260a74209ded
