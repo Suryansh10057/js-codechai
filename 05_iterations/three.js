86d7c41336a283e4e634902d8bb4b10ea7e7e6c4
Suryansh10057 <139328478+Suryansh10057@users.noreply.github.com>
2025-05-19 19:39:48 +0000

    loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,44 @@
+// 
+
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]  //array of object --foreach handles this
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()    //map is also a object hold key value pair
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// objects are not iterable like maps as given below
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
